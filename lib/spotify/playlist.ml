787eb86872a481d07783c67e8482ecc93fc59c63
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-06-05 21:08:52 -0400

    add initial playlists module + Me api

--- lib/spotify/playlist.ml
+++ lib/spotify/playlist.ml
@@ -1,1 +1,7 @@
-type t
+type t = unit
+
+module Me = struct
+  let get_playlists _client = Lwt.return_ok ()
+end
+
+let get_featured_playlists _client = Lwt.return_ok ()
--- lib/spotify/playlist.mli
+++ lib/spotify/playlist.mli
@@ -1,1 +1,9 @@
 type t
+
+module Me : sig
+  val get_playlists : Client.t -> (t, [ `Msg of string ]) result Lwt.t
+  (* Spotify.Playlist.Me.get_playlists *)
+end
+
+val get_featured_playlists : Client.t -> (t, [ `Msg of string ]) result Lwt.t
+(* Spotify.Playlist.get_featured_playlists *)
