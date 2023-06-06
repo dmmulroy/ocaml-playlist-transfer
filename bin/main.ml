13dc9fec2355d8daeba07a487cd22f8b44f29f47
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-06-06 08:22:53 -0400

    wip

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,4 @@
-let () =
+let _ =
   print_newline ();
   let client_id = Sys.getenv "SPOTIFY_CLIENT_ID" in
   let client_secret = Sys.getenv "SPOTIFY_CLIENT_SECRET" in
@@ -6,10 +6,9 @@ let () =
     match%lwt
       Spotify.Authorization.fetch_access_token ~client_id ~client_secret
     with
-    | Ok access_token ->
-        Lwt.return @@ print_endline @@ "access token: "
-        ^ Spotify.Authorization.Access_token.show access_token
-    | Error err ->
-        Lwt.return @@ print_endline @@ Spotify.Error.to_human_string err
+    | Ok access_token -> Lwt.return access_token
+    | Error err -> Lwt.fail_with @@ Spotify.Error.to_human_string err
   in
-  Lwt_main.run access_token_promise
+  let token = Lwt_main.run access_token_promise in
+  print_endline @@ "Access token: "
+  ^ Spotify.Authorization.Access_token.show token
