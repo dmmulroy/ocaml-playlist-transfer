8b697d35c46925a631b041a151f4f5aeffade984
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-06-21 20:30:31 -0400

    wip on user

--- lib/spotify/user.ml
+++ lib/spotify/user.ml
@@ -19,28 +19,36 @@ type t = {
 }
 [@@deriving yojson]
 
-type pub = {
-  display_name : string option; [@default None]
-  external_urls : Common.external_urls;
-  followers : Resource_type.reference option; [@default None]
-  href : Http.Uri.t;
-  id : string;
-  image : Common.image list option; [@default None]
-  resource_type : resource_type; [@key "type"]
-  uri : Uri.t;
-}
-[@@deriving yojson]
+module Me = struct
+  type product = [ `Premium | `Free | `Open ]
 
-type priv = {
-  display_name : string option; [@default None]
-  external_urls : Common.external_urls;
-  followers : Resource_type.reference option; [@default None]
-  href : Http.Uri.t;
-  id : string;
-  image : Common.image list option; [@default None]
-  resource_type : resource_type; [@key "type"]
-  uri : Uri.t;
-}
-[@@deriving yojson]
+  type explicit_content = { filter_enabled : bool; filter_locked : bool }
+  [@@deriving yojson]
 
-type t2 = [ `Public of pub | `Private of priv ]
+  let product_of_yojson = function
+    | `String "premium" -> Ok `Premium
+    | `String "free" -> Ok `Free
+    | `String "open" -> Ok `Open
+    | _ -> Error "Invalid product"
+
+  let product_to_yojson = function
+    | `Premium -> `String "premium"
+    | `Free -> `String "free"
+    | `Open -> `String "open"
+
+  type t = {
+    country : string;
+    email : string option; [@default None]
+    explicit_content : explicit_content option; [@default None]
+    product : product;
+    display_name : string option; [@default None]
+    external_urls : Common.external_urls;
+    followers : Resource_type.reference option; [@default None]
+    href : Http.Uri.t;
+    id : string;
+    image : Common.image list option; [@default None]
+    resource_type : resource_type; [@key "type"]
+    uri : Uri.t;
+  }
+  [@@deriving yojson]
+end
