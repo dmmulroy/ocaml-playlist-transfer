db77f8e68b355e243afa096936a7029a9e6e1a88
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-06-21 20:47:31 -0400

    remove majority of resource_type code

--- lib/spotify/resource_type.mli
+++ lib/spotify/resource_type.mli
@@ -1,19 +1,1 @@
-type t = [ `Album | `Artist | `Episode | `Playlist | `Track | `User ]
-[@@deriving yojson]
-
 type reference = { href : Http.Uri.t option; total : int } [@@deriving yojson]
-
-val album_of_yojson : Yojson.Safe.t -> ([ `Album ], string) result
-val album_to_yojson : [ `Album ] -> Yojson.Safe.t
-val artist_of_yojson : Yojson.Safe.t -> ([ `Artist ], string) result
-val artist_to_yojson : [ `Artist ] -> Yojson.Safe.t
-val episode_of_yojson : Yojson.Safe.t -> ([ `Episode ], string) result
-val episode_to_yojson : [ `Episode ] -> Yojson.Safe.t
-val playlist_of_yojson : Yojson.Safe.t -> ([ `Playlist ], string) result
-val playlist_to_yojson : [ `Playlist ] -> Yojson.Safe.t
-val track_of_yojson : Yojson.Safe.t -> ([ `Track ], string) result
-val track_to_yojson : [ `Track ] -> Yojson.Safe.t
-val user_of_yojson : Yojson.Safe.t -> ([ `User ], string) result
-val user_to_yojson : [ `User ] -> Yojson.Safe.t
-val of_string : string -> t
-val to_string : [< t ] -> string
--- lib/spotify/uri.ml
+++ lib/spotify/uri.ml
@@ -1,13 +1,12 @@
-type t = { resource_type : Resource_type.t; id : string }
+(* TODO Thursday: Figure out better way to type 'resource_type'*)
+type t = { resource_type : string; id : string }
 
 let of_string str =
   match String.split_on_char ':' str with
-  | [ _; resource_type; id ] ->
-      { resource_type = Resource_type.of_string resource_type; id }
+  | [ _; resource_type; id ] -> { resource_type; id }
   | _ -> failwith @@ "Invalid Spotify URI" ^ str
 
-let to_string t =
-  Printf.sprintf "spotify:%s:%s" (Resource_type.to_string t.resource_type) t.id
+let to_string t = Printf.sprintf "spotify:%s:%s" t.resource_type t.id
 
 let of_yojson = function
   | `String str -> Ok (of_string str)
