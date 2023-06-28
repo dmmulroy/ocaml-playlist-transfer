e935e44767fb0dda145c3c188d260ccc80d0bb88
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-06-28 10:01:46 -0400

    add todo comment for tomorrow

--- lib/spotify/client.mli
+++ lib/spotify/client.mli
@@ -2,3 +2,8 @@ type t
 
 val get_bearer_token : t -> string
 val make : Authorization.Access_token.t -> t
+
+(*
+    TODO Thursday: Migrate to from cohttp_lwt_unix to Dream.hyper and design the
+    client to execute api calls
+*)
