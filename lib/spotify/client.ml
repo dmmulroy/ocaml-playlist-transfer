7978ea360d78d8adb0fdb7378a0d84c0c3200fc1
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-06-29 11:09:09 -0400

    wip on new client req/res model

--- lib/spotify/client.ml
+++ lib/spotify/client.ml
@@ -2,3 +2,62 @@ type t = Authorization.Access_token.t
 
 let get_bearer_token t = Authorization.Access_token.to_bearer_token t
 let make access_token = access_token
+
+type 'a promise = 'a Lwt.t
+
+module HttpRequest = struct
+  type t = method_ * headers * endpoint * body
+
+  and method_ =
+    [ `GET
+    | `POST
+    | `PUT
+    | `DELETE
+    | `HEAD
+    | `CONNECT
+    | `OPTIONS
+    | `TRACE
+    | `PATCH ]
+
+  and headers = (string * string) list
+  and endpoint = Uri.t
+  and body = string option
+
+  (* val method_to_string : [< method_ ] -> string *)
+  (* val method_of_string : string -> method_ *)
+end
+
+module HttpResponse = struct
+  type t = Http.Response.t * Http.Body.t
+end
+
+module type SpotifyRequest = sig
+  type input
+  type output
+  type error
+
+  val to_http : input -> HttpRequest.t
+  val of_http : HttpResponse.t -> (output, error) result
+end
+
+type ('input, 'output, 'error) request =
+  (module SpotifyRequest
+     with type input = 'input
+      and type output = 'output
+      and type error = 'error)
+
+let execute_request (type input output error)
+    (module M : SpotifyRequest
+      with type input = input
+       and type output = output
+       and type error = error) (t : t) (input : M.input) =
+  let method_, headers, endpoint, _body = M.to_http input in
+  match method_ with
+  | `GET ->
+      let headers = ("Authorization", get_bearer_token t) :: headers in
+      let%lwt response =
+        Http.Client.get ~headers:(Http.Header.of_list headers) endpoint
+      in
+      Lwt.return (M.of_http response)
+  | _ -> failwith "Not implemented"
+(* TODO Friday: Continue implementing execute_reqeust *)
--- lib/spotify/client.mli
+++ lib/spotify/client.mli
@@ -4,6 +4,69 @@ val get_bearer_token : t -> string
 val make : Authorization.Access_token.t -> t
 
 (*
-    TODO Thursday: Migrate to from cohttp_lwt_unix to Dream.hyper and design the
-    client to execute api calls
+  Spotify.Playlist.get_by_id ~client:client ~id:"123" ()
 *)
+
+module HttpRequest : sig
+  type t = method_ * headers * endpoint * body
+
+  and method_ =
+    [ `GET
+    | `POST
+    | `PUT
+    | `DELETE
+    | `HEAD
+    | `CONNECT
+    | `OPTIONS
+    | `TRACE
+    | `PATCH ]
+
+  and headers = (string * string) list
+  and endpoint = Uri.t
+  and body = string option
+
+  (* val method_to_string : [< method_ ] -> string *)
+  (* val method_of_string : string -> method_ *)
+end
+
+module HttpResponse : sig
+  type t
+end
+
+module type SpotifyRequest = sig
+  type input
+  type output
+  type error
+
+  val to_http : input -> HttpRequest.t
+  val of_http : HttpResponse.t -> (output, error) result
+end
+
+type ('input, 'output, 'error) request =
+  (module SpotifyRequest
+     with type input = 'input
+      and type output = 'output
+      and type error = 'error)
+
+type 'a promise = 'a Lwt.t
+
+val execute_request :
+  ('input, 'output, 'error) request ->
+  t ->
+  'input ->
+  ('output, 'error) result promise
+
+(* module GetPlaylistByid : sig *)
+(*   type input = string *)
+(*   type output = string *)
+(*   type error = string *)
+(***)
+(*   val to_http : input -> Http.HttpRequest.t *)
+(*   val of_http : Http.HttpResponse.t -> (output, error) result *)
+(***)
+(*   include *)
+(*     SpotifyRequest *)
+(*       with type input := input *)
+(*        and type output := output *)
+(*        and type error := error *)
+(* end *)
