688cece463322e24568e8c807e101b29a7399d73
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-07-09 21:27:29 +0100

    remove commented code

--- lib/spotify/client.mli
+++ lib/spotify/client.mli
@@ -1,8 +1,5 @@
 type t
 
-(* val get_bearer_token : t -> (string, [ `Unauthenticated ]) result *)
 val get_bearer_token : t -> string
-
-(* val init : ?access_token:Access_token.t -> unit -> t *)
 val make : Access_token.t -> t
 val set_access_token : t -> Access_token.t -> t
