069d31e79afc0491b83ea3ca698b8df4691fed91
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-07-28 20:20:53 -0400

    add is_expired

--- lib/apple/authorization.ml
+++ lib/apple/authorization.ml
@@ -1,8 +1,20 @@
-module Jwt = struct
+module Jwt : sig
+  type t
+
+  val is_expired : t -> bool
+  val make : unit -> unit
+  val to_string : t -> string
+  val validate : t -> t
+end = struct
   type t = Jose.Jwt.t
 
-  let is_expired _t = false (* TODO *)
+  let is_expired t =
+    let expiration_result =
+      Jose.Jwt.check_expiration ~now:(Ptime_clock.now ()) t
+    in
+    match expiration_result with Ok _ -> false | Error _ -> true
+
   let make () = () (* TODO *)
-  let to_string t = Jose.Jwt.to_string t (* TODO *)
+  let to_string t = Jose.Jwt.to_string t
   let validate t = t (* TODO *)
 end
