9d18f8f31cfa69cae69bf8fb957c8bd8faf7a3c0
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-08-02 10:44:49 -0400

    wip

--- lib/spotify/error.ml
+++ lib/spotify/error.ml
@@ -1,8 +1,7 @@
-type t = [ Authorization.error | Common.error | `Unknown_error ]
+type t = [ Authorization.error | Common.error ]
 
 let to_string (err : [< t ]) =
   match err with
   | #Common.error as err -> Common.error_to_string err
   | #Authorization.error as err -> Authorization.error_to_string err
-  | `Unknown_error -> "Unknown error"
   | #t -> .
