5230da060915386c83c4a2bfaef10783b8930327
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-08-05 19:33:21 -0400

    it's all working again - hazahhhh

--- lib/apple/error.ml
+++ lib/apple/error.ml
@@ -1,14 +1,5 @@
-type t = [ Song.error | `Json_parse_error ]
-
-let to_string (err : [< t ]) =
-  match err with
-  | #Song.error as err -> Song.error_to_string err
-  (* | #Apple_request.error as err -> Apple_request.error_to_string err *)
-  | `Json_parse_error -> "JSON parse error"
-  | #t -> .
-
 module type S = sig
-  type t
+  type t = private [> ]
 
   val to_string : t -> string
 end
@@ -18,3 +9,42 @@ module Make (M : S) : S with type t = M.t = struct
 
   let to_string = M.to_string
 end
+
+module Http_error = Make (struct
+  type t = [ `Http_error of int * string ]
+
+  let to_string = function
+    | `Http_error (code, msg) -> Printf.sprintf "HTTP error %d: %s" code msg
+end)
+
+module Json_parse_error = Make (struct
+  type t = [ `Json_parse_error of string ]
+
+  let to_string = function
+    | `Json_parse_error msg -> Printf.sprintf "JSON parse error: %s" msg
+end)
+
+module Song_error = Make (struct
+  type t = [ `Song_not_found | Http_error.t ]
+
+  let to_string = function
+    | `Song_not_found -> "Song not found"
+    | #Http_error.t as err -> Http_error.to_string err
+end)
+
+module Authorization_error = Make (struct
+  type t = Http_error.t
+
+  let to_string = function #Http_error.t as err -> Http_error.to_string err
+end)
+
+type t =
+  [ Http_error.t | Song_error.t | Authorization_error.t | Json_parse_error.t ]
+
+let to_string (err : [< t ]) =
+  match err with
+  | #Http_error.t as err -> Http_error.to_string err
+  (* | #Authorization_error.t as err -> Authorization_error.to_string err *)
+  | #Json_parse_error.t as err -> Json_parse_error.to_string err
+  | #Song_error.t as err -> Song_error.to_string err
+  | #t -> .
--- lib/apple/song.mli
+++ lib/apple/song.mli
@@ -1,7 +1,4 @@
 type t = string
-type error = [ `Song_not_found ]
-
-val error_to_string : error -> string
 
 module Get_song_by_id_input : sig
   type t = string
@@ -11,11 +8,6 @@ module Get_song_by_id_output : sig
   type nonrec t = t
 end
 
-module Get_song_by_id_error : sig
-  type t = error
-
-  val to_string : t -> string
-end
-
 val get_song_by_id :
-  Get_song_by_id_input.t -> (Get_song_by_id_output.t, [> error ]) Lwt_result.t
+  Get_song_by_id_input.t ->
+  (Get_song_by_id_output.t, Error.Song_error.t) Lwt_result.t
