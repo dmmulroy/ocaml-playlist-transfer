b3ffe9e612d6c63d9d25349a3ef9833612a55944
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-08-06 18:14:00 -0400

    continue error simplicity refactor

--- lib/spotify/error.ml
+++ lib/spotify/error.ml
@@ -1,7 +1,14 @@
-type t = [ Authorization.error | Common.error ]
+type t =
+  [ `Json_parse_error of string
+  | `No_refresh_token
+  | `Invalid_grant_type
+  | `Http_error of int * string ]
 
 let to_string (err : [< t ]) =
   match err with
-  | #Common.error as err -> Common.error_to_string err
-  | #Authorization.error as err -> Authorization.error_to_string err
+  | `Json_parse_error msg -> "Error parsing JSON response: " ^ msg
+  | `Http_error (status_code, msg) ->
+      Printf.sprintf "Request error: [%d]: %s" status_code msg
+  | `No_refresh_token -> "No refresh token"
+  | `Invalid_grant_type -> "Invalid grant type"
   | #t -> .
