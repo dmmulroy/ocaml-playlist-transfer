26c21552e81fac91cfb23ff6bf133dec5060aba7
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-08-07 09:39:27 -0400

    wip

--- lib/apple/error.ml
+++ lib/apple/error.ml
@@ -3,6 +3,10 @@ type t =
 
 let to_string (err : [< t ]) =
   match err with
+  (*
+    TODO: Fix Http_error to_string
+    ex output: Fatal error: exception Failure("HTTP error 401: ")
+   *)
   | `Http_error (code, msg) -> Printf.sprintf "HTTP error %d: %s" code msg
   | `Json_parse_error msg -> Printf.sprintf "JSON parse error: %s" msg
   | `Song_not_found -> "Song not found"
