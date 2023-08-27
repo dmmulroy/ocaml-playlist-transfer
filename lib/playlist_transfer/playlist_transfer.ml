4ac71be0435634445fb8436116e22baf6667b9d4
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-08-27 12:38:18 -0400

    playing with GADTs for modeling transfers

--- lib/playlist_transfer/playlist_transfer.ml
+++ lib/playlist_transfer/playlist_transfer.ml
@@ -1,14 +1,24 @@
-type apple = Apple
-type spotify = Spotify
+type apple = Apple of string
+type spotify = Spotify of int
 
 type _ playlist =
-  | ApplePlaylist : apple playlist
-  | SpotifyPlaylist : spotify playlist
+  | ApplePlaylist : apple -> apple playlist
+  | SpotifyPlaylist : spotify -> spotify playlist
 
 type _ converted_playlist =
   | AppleOfSpotify : apple playlist -> spotify converted_playlist
   | SpotifyOfApple : spotify playlist -> apple converted_playlist
 
 let convert : type a. a playlist -> a converted_playlist = function
-  | ApplePlaylist -> SpotifyOfApple SpotifyPlaylist
-  | SpotifyPlaylist -> AppleOfSpotify ApplePlaylist
+  | ApplePlaylist (Apple str) ->
+      SpotifyOfApple (SpotifyPlaylist (Spotify (int_of_string str)))
+  | SpotifyPlaylist (Spotify num) ->
+      AppleOfSpotify (ApplePlaylist (Apple (string_of_int num)))
+
+let _ =
+  let ap = Apple "apple" in
+  let playlist = ApplePlaylist ap in
+  let converted = convert playlist in
+  match converted with
+  | SpotifyOfApple (SpotifyPlaylist (Spotify num)) ->
+      print_endline (string_of_int num)
