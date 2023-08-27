b9731d0e8de026b31c58b44b96fda8c6393aa535
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-08-27 12:54:47 -0400

    playing with GADTs for modeling transfers

--- lib/playlist_transfer/playlist_transfer.ml
+++ lib/playlist_transfer/playlist_transfer.ml
@@ -1,24 +1,41 @@
-type apple = Apple of string
-type spotify = Spotify of int
-
 type _ playlist =
-  | ApplePlaylist : apple -> apple playlist
-  | SpotifyPlaylist : spotify -> spotify playlist
+  | Apple : Apple.Library_playlist.t -> Apple.Library_playlist.t playlist
+  | Spotify : Spotify.Playlist.t -> Spotify.Playlist.t playlist
 
 type _ converted_playlist =
-  | AppleOfSpotify : apple playlist -> spotify converted_playlist
-  | SpotifyOfApple : spotify playlist -> apple converted_playlist
+  | AppleOfSpotify :
+      Apple.Library_playlist.t playlist
+      -> Spotify.Playlist.t converted_playlist
+  | SpotifyOfApple :
+      Spotify.Playlist.t playlist
+      -> Apple.Library_playlist.t converted_playlist
+
+let convert_apple_to_spotify (_playlist : Apple.Library_playlist.t) :
+    Spotify.Playlist.t =
+  failwith "TODO"
+
+let convert_spotify_to_apple (_playlist : Spotify.Playlist.t) :
+    Apple.Library_playlist.t =
+  failwith "TODO"
+
+let make_apple_playlist () : Apple.Library_playlist.t playlist = failwith "TODO"
+let make_spotify_playlist () : Spotify.Playlist.t playlist = failwith "TODO"
 
 let convert : type a. a playlist -> a converted_playlist = function
-  | ApplePlaylist (Apple str) ->
-      SpotifyOfApple (SpotifyPlaylist (Spotify (int_of_string str)))
-  | SpotifyPlaylist (Spotify num) ->
-      AppleOfSpotify (ApplePlaylist (Apple (string_of_int num)))
+  | Apple playlist ->
+      SpotifyOfApple (Spotify (convert_apple_to_spotify playlist))
+  | Spotify playlist ->
+      AppleOfSpotify (Apple (convert_spotify_to_apple playlist))
 
-let _ =
-  let ap = Apple "apple" in
-  let playlist = ApplePlaylist ap in
-  let converted = convert playlist in
-  match converted with
-  | SpotifyOfApple (SpotifyPlaylist (Spotify num)) ->
-      print_endline (string_of_int num)
+let get_spotify_playlist = function
+  | SpotifyOfApple (Spotify playlist) -> playlist
+
+let get_apple_playlist = function AppleOfSpotify (Apple playlist) -> playlist
+
+let spotify_playlist =
+  let apple_playlist = make_apple_playlist () in
+  convert apple_playlist |> get_spotify_playlist
+
+let apple_playlist =
+  let spotify_playlist = make_spotify_playlist () in
+  convert spotify_playlist |> get_apple_playlist
