447ce9d2f74202a9ccc5bdd4820c4ccf7fc38c55
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-08-27 13:59:11 -0400

    playing with GADTs for modeling transfers

--- lib/playlist_transfer/playlist_transfer.ml
+++ lib/playlist_transfer/playlist_transfer.ml
@@ -1,3 +1,5 @@
+type _ x = Int : int -> string x | String : string -> int x
+
 type _ playlist =
   | Apple : Apple.Library_playlist.t -> Apple.Library_playlist.t playlist
   | Spotify : Spotify.Playlist.t -> Spotify.Playlist.t playlist
@@ -39,3 +41,34 @@ let spotify_playlist =
 let apple_playlist =
   let spotify_playlist = make_spotify_playlist () in
   convert spotify_playlist |> get_apple_playlist
+
+type _ int_or_string = Int : int int_or_string | String : string int_or_string
+
+let get_value : type a. a int_or_string -> a = function
+  | Int -> Random.int 101
+  | String -> string_of_int (Random.int 101)
+
+let value = get_value Int (* Some Random Int between 0 and 100  *)
+let value = get_value String (* Some Random String *)
+
+type _ int_or_string =
+  | Int : int -> int int_or_string
+  | String : string -> string int_or_string
+
+let double : type a. a int_or_string -> a = function
+  | Int i -> i * 2
+  | String s -> s ^ s
+
+let value = double (Int 2) (* 4 *)
+let value = double (String "hello") (* "hellohello" *)
+
+type _ int_or_string =
+  | Int : int -> string int_or_string
+  | String : string -> int int_or_string
+
+let transform : type a. a int_or_string -> a = function
+  | Int i -> string_of_int i
+  | String s -> int_of_string s
+
+let value = transform (Int 2) (* "2" *)
+let value = transform (String "2") (* 2 *)
