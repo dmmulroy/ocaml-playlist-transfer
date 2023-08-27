a5e14916ba7295796d819ad8ebaece8f0e00148c
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-08-27 12:14:52 -0400

    playing with GADTs for modeling transfers

--- lib/playlist_transfer/playlist_transfer.ml
+++ lib/playlist_transfer/playlist_transfer.ml
@@ -1,2 +1,14 @@
-type playlist =
-  [ `Apple of Apple.Library_playlist.t | `Spotify of Spotify.Playlist.t ]
+type apple = Apple
+type spotify = Spotify
+
+type _ playlist =
+  | ApplePlaylist : apple playlist
+  | SpotifyPlaylist : spotify playlist
+
+type _ converted_playlist =
+  | AppleOfSpotify : apple playlist -> spotify converted_playlist
+  | SpotifyOfApple : spotify playlist -> apple converted_playlist
+
+let convert : type a. a playlist -> a converted_playlist = function
+  | ApplePlaylist -> SpotifyOfApple SpotifyPlaylist
+  | SpotifyPlaylist -> AppleOfSpotify ApplePlaylist
