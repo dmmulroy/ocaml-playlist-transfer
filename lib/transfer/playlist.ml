00bb76804852a038fa5f22d4beec9c1b221ec95e
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-09-05 22:20:02 -0400

    add of_spotify and of_apple to Transfer.Playlist

--- lib/transfer/playlist.ml
+++ lib/transfer/playlist.ml
@@ -1,1 +1,13 @@
-type t = { description : string; name : string }
+type t = { description : string; name : string; tracks : Track.t list option }
+
+let of_apple (playlist : Apple.Library_playlist.t) =
+  let description =
+    Option.fold ~none:playlist.attributes.name
+      ~some:(fun (description : Apple.Description.t) -> description.standard)
+      playlist.attributes.description
+  in
+  { description; name = playlist.attributes.name; tracks = None }
+
+let of_spotify (playlist : Spotify.Playlist.t) =
+  let description = Option.value ~default:playlist.name playlist.description in
+  { description; name = playlist.name; tracks = None }
