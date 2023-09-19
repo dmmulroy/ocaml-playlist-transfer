f41992635f0c6b392b2dd0e8c7364fd9a91bde1d
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-09-19 08:49:40 -0400

    add extended lib + list module

--- lib/apple/song.ml
+++ lib/apple/song.ml
@@ -179,21 +179,8 @@ module Get_many_by_isrcs = Apple_request.Make (struct
     Apple_request.handle_response ~deserialize:output_of_yojson
 end)
 
-(* (val chunk: int -> 'a list -> 'a list list) *)
-let chunk (chunk_size : int) (list : 'a list) =
-  let rec aux (acc : 'a list list) (chunk : 'a list) (current_chunk_size : int)
-      (list' : 'a list) =
-    match list' with
-    | [] -> if chunk = [] then acc else List.rev chunk :: acc
-    | hd :: tl ->
-        if current_chunk_size < chunk_size then
-          aux acc (hd :: chunk) (current_chunk_size + 1) tl
-        else aux (List.rev chunk :: acc) [ hd ] 1 tl
-  in
-  List.rev (aux [] [] 0 list)
-
 let get_many_by_isrcs ~(client : Client.t) (input : Get_many_by_isrcs_input.t) =
-  let chunked_isrcs = chunk 25 input in
+  let chunked_isrcs = Extended.List.chunk 25 input in
   let promises =
     List.map
       (fun isrcs -> Get_many_by_isrcs.request ~client isrcs)
---
+++ lib/extended/extended.ml
@@ -0,0 +1,14 @@
+module List = struct
+  (* (val chunk: int -> 'a list -> 'a list list) *)
+  let chunk (chunk_size : int) (list : 'a list) =
+    let rec aux (acc : 'a list list) (chunk : 'a list)
+        (current_chunk_size : int) (list' : 'a list) =
+      match list' with
+      | [] -> if chunk = [] then acc else List.rev chunk :: acc
+      | hd :: tl ->
+          if current_chunk_size < chunk_size then
+            aux acc (hd :: chunk) (current_chunk_size + 1) tl
+          else aux (List.rev chunk :: acc) [ hd ] 1 tl
+    in
+    List.rev (aux [] [] 0 list)
+end
