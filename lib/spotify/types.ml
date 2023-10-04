116059eacad5c7802b51d0feeb4542a80aec317d
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-10-04 07:39:57 -0400

    refactor spotify domain types to types.ml

--- lib/spotify/types.ml
+++ lib/spotify/types.ml
@@ -1,15 +1,92 @@
 open Shared
 
-(* TODO Wednesday: Move the Common module into this module *)
+module Copyright = struct
+  type t = [ `C of string | `P of string ] [@@deriving yojson]
+
+  let of_yojson = function
+    | `Assoc [ ("text", `String s); ("type", `String "C") ] -> Ok (`C s)
+    | `Assoc [ ("text", `String s); ("type", `String "P") ] -> Ok (`P s)
+    | _ -> Error "Invalid copyright"
+
+  let to_yojson = function
+    | `C s -> `Assoc [ ("text", `String s); ("type", `String "C") ]
+    | `P s -> `Assoc [ ("text", `String s); ("type", `String "P") ]
+end
+
+module External = struct
+  type ids = {
+    ean : string option; [@default None]
+    isrc : string option; [@default None]
+    spotify : string option; [@default None]
+    upc : string option; [@default None]
+  }
+  [@@deriving yojson]
+
+  type urls = { spotify : string } [@@deriving yojson]
+end
+
+module Image = struct
+  type t = { height : int option; url : Http.Uri.t; width : int option }
+  [@@deriving yojson]
+end
+
+module Reference = struct
+  type t = { href : Http.Uri.t option; total : int } [@@deriving yojson]
+end
+
+module Release_date_precision = struct
+  type t = [ `Year | `Month | `Day ] [@@deriving yojson]
+
+  let of_yojson = function
+    | `String "year" -> Ok `Year
+    | `String "month" -> Ok `Month
+    | `String "day" -> Ok `Day
+    | _ -> Error "Invalid album release_date_precision"
+
+  let to_yojson = function
+    | `Year -> `String "year"
+    | `Month -> `String "month"
+    | `Day -> `String "day"
+end
+
+module Restriction = struct
+  type restriction_reason = [ `Market | `Product | `Explicit ]
+  [@@deriving yojson]
+
+  let restriction_reason_of_yojson = function
+    | `String "market" -> Ok `Market
+    | `String "product" -> Ok `Product
+    | `String "explicit" -> Ok `Explicit
+    | _ -> Error "Invalid album restrictions_reason"
+
+  let restriction_reason_to_yojson = function
+    | `Market -> `String "market"
+    | `Product -> `String "product"
+    | `Explicit -> `String "explicit"
+    | #restriction_reason -> .
+
+  type t = { reason : restriction_reason } [@@deriving yojson]
+end
+
+module Linked_track = struct
+  type t = {
+    external_urls : External.urls;
+    href : Http.Uri.t;
+    id : string;
+    resource_type : Resource.t; [@key "type"]
+    uri : string;
+  }
+  [@@deriving yojson]
+end
 
 module Artist = struct
   type t = {
-    external_urls : Common.external_urls;
+    external_urls : External.urls;
     followers : Resource.reference;
     genres : string list;
     href : Http.Uri.t;
     id : string;
-    images : Common.image list;
+    images : Image.t list;
     name : string;
     popularity : int;
     resource_type : Resource.t; [@key "type"]
@@ -20,7 +97,7 @@ end
 
 module Simple_artist = struct
   type t = {
-    external_urls : Common.external_urls;
+    external_urls : External.urls;
     href : Http.Uri.t;
     id : string;
     name : string;
@@ -37,16 +114,16 @@ module Simple_track = struct
     disc_number : int;
     duration_ms : int;
     explicit : bool;
-    external_urls : Common.external_urls;
+    external_urls : External.urls;
     href : Http.Uri.t;
     id : string;
     is_local : bool;
     is_playable : bool option;
-    linked_from : Common.linked_track option;
+    linked_from : Linked_track.t option;
     name : string;
     preview_url : string option;
     resource_type : Resource.t; [@key "type"]
-    restrictions : Common.restriction list option;
+    restrictions : Restriction.t list option;
     track_number : int;
     uri : string;
   }
@@ -83,19 +160,19 @@ module Album = struct
     album_type : album_type;
     artists : Artist.t list;
     available_markets : string list;
-    copyrights : Common.copyright list;
-    external_urls : Common.external_urls;
+    copyrights : Copyright.t list;
+    external_urls : External.urls;
     genres : string list;
     href : Http.Uri.t;
     id : string;
-    images : Common.image list;
+    images : Image.t list;
     label : string;
     name : string;
     popularity : int;
     release_date : string;
-    release_date_precision : Common.release_date_precision;
+    release_date_precision : Release_date_precision.t;
     resource_type : Resource.t; [@key "type"]
-    restrictions : Common.restriction list option; [@default None]
+    restrictions : Restriction.t list option; [@default None]
     total_tracks : int;
     tracks : Simple_track.t list;
     uri : string;
@@ -112,11 +189,11 @@ module Episode = struct
     description : string option; [@default None]
     duration_ms : int;
     explicit : bool;
-    external_urls : Common.external_urls;
+    external_urls : External.urls;
     href : Http.Uri.t;
     html_description : string option; [@default None]
     id : string;
-    images : Common.image list option; [@default None]
+    images : Image.t list option; [@default None]
     is_externally_hosted : bool option; [@default None]
     is_playable : bool;
     languages : string list option; [@default None]
@@ -124,7 +201,7 @@ module Episode = struct
     release_date : string option; [@default None]
     release_date_precision : string option; [@default None]
     resource_type : Resource.t; [@key "type"]
-    restrictions : Common.restriction option; [@default None]
+    restrictions : Restriction.t option; [@default None]
     resume_point : resume_point option; [@default None]
     uri : string;
   }
@@ -154,11 +231,11 @@ module Private_user = struct
     explicit_content : explicit_content option; [@default None]
     product : product;
     display_name : string option; [@default None]
-    external_urls : Common.external_urls;
+    external_urls : External.urls;
     followers : Resource.reference option; [@default None]
     href : Http.Uri.t;
     id : string;
-    image : Common.image list option; [@default None]
+    image : Image.t list option; [@default None]
     resource_type : Resource.t; [@key "type"]
     uri : string;
   }
@@ -171,16 +248,15 @@ module Simple_album = struct
     album_type : Album.album_type;
     artists : Simple_artist.t list;
     available_markets : string list;
-    external_urls : Common.external_urls;
+    external_urls : External.urls;
     href : Http.Uri.t;
     id : string;
-    images : Common.image list;
+    images : Image.t list;
     is_playable : bool option; [@default None]
     name : string;
     release_date : string option; [@default None]
-    release_date_precision : Common.release_date_precision option;
-        [@default None]
-    restrictions : Common.restriction option; [@default None]
+    release_date_precision : Release_date_precision.t option; [@default None]
+    restrictions : Restriction.t option; [@default None]
     total_tracks : int;
     resource_type : Resource.t; [@key "type"]
     uri : string;
@@ -194,11 +270,11 @@ module Simple_episode = struct
     description : string;
     duration_ms : int;
     explicit : bool;
-    external_urls : Common.external_urls;
+    external_urls : External.urls;
     href : Http.Uri.t;
     html_description : string;
     id : string;
-    images : Common.image list;
+    images : Image.t list;
     is_externally_hosted : bool;
     is_playable : bool;
     languages : string list;
@@ -206,7 +282,7 @@ module Simple_episode = struct
     release_date : string;
     release_date_precision : string;
     resource_type : Resource.t; [@key "type"]
-    restrictions : Common.restriction option; [@default None]
+    restrictions : Restriction.t option; [@default None]
     resume_point : Episode.resume_point;
     uri : string;
   }
@@ -216,14 +292,14 @@ end
 module Simple_show = struct
   type t = {
     available_markets : string list;
-    copyrights : Common.copyright list;
+    copyrights : Copyright.t list;
     description : string;
     explicit : bool;
-    external_urls : Common.external_urls;
+    external_urls : External.urls;
     href : Http.Uri.t;
     html_description : string;
     id : string;
-    images : Common.image list;
+    images : Image.t list;
     is_externally_hosted : bool;
     languages : string list;
     media_type : string;
@@ -239,11 +315,11 @@ end
 module User = struct
   type t = {
     display_name : string option; [@default None]
-    external_urls : Common.external_urls;
+    external_urls : External.urls;
     followers : Resource.reference option; [@default None]
     href : Http.Uri.t;
     id : string;
-    image : Common.image list option; [@default None]
+    image : Image.t list option; [@default None]
     resource_type : Resource.t; [@key "type"]
     uri : string;
   }
@@ -254,10 +330,10 @@ module Simple_playlist = struct
   type t = {
     collaborative : bool;
     description : string option;
-    external_urls : Common.external_urls;
+    external_urls : External.urls;
     href : Http.Uri.t;
     id : string;
-    images : Common.image list;
+    images : Image.t list;
     name : string;
     owner : User.t;
     public : bool option;
@@ -272,15 +348,15 @@ end
 module Show = struct
   type t = {
     available_markets : string list;
-    copyrights : Common.copyright list;
+    copyrights : Copyright.t list;
     description : string;
     episodes : Simple_episode.t list;
     explicit : bool;
-    external_urls : Common.external_urls;
+    external_urls : External.urls;
     href : Http.Uri.t;
     html_description : string;
     id : string;
-    images : Common.image list;
+    images : Image.t list;
     is_externally_hosted : bool;
     languages : string list;
     media_type : string;
@@ -302,18 +378,18 @@ module Track = struct
     duration_ms : int;
     episode : bool option; [@default None]
     explicit : bool;
-    external_ids : Common.external_ids;
-    external_urls : Common.external_urls;
+    external_ids : External.ids;
+    external_urls : External.urls;
     href : Http.Uri.t;
     id : string;
     is_local : bool;
     is_playable : bool option; [@default None]
-    (* linked_from : Common.linked_track option; [@default None] *)
+    (* linked_from : Linked_track.t option; [@default None] *)
     name : string;
     popularity : int;
     preview_url : string option; [@default None]
     resource_type : Resource.t; [@key "type"]
-    (* restrictions : Common.restriction list option; [@default None] *)
+    (* restrictions : Restriction.t list option; [@default None] *)
     track : bool option; [@default None]
     track_number : int;
     uri : string;
@@ -337,11 +413,11 @@ module Playlist = struct
   type t = {
     collaborative : bool;
     description : string option; [@default None]
-    external_urls : Common.external_urls;
+    external_urls : External.urls;
     followers : Resource.reference;
     href : Http.Uri.t;
     id : string;
-    images : Common.image list;
+    images : Image.t list;
     name : string;
     owner : User.t;
     primary_color : string option; [@default None]
